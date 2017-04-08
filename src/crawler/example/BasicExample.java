553ec44b94c948488f366fca016b429b66b08ef5
wendy971 <wendy971twtw@gmail.com>
2017-04-08 10:35:25 +0800

    確認可上傳GitHub

--- src/crawler/example/BasicExample.java
+++ src/crawler/example/BasicExample.java
@@ -11,6 +11,7 @@ import org.apache.commons.logging.impl.SimpleLog;
  */
 public class BasicExample {
 	// commit test
+	// wendy test1
 	public static void main(String[] args) {
 
 		// set to debug level
